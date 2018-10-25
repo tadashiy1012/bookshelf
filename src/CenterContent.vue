3b436cea5ebfdb4ff4b0cb44c225dfa81a3e4e76
yama3ki <yama3ki.tada4@gmail.com>
2018-10-25 23:15:49 +0900

    dropareaを分離

--- src/CenterContent.vue
+++ src/CenterContent.vue
@@ -3,27 +3,22 @@
         <h2>category:<span>{{category}}</span></h2>
         <button v-on:click="onToggleRight">add book</button>
         <p>{{books.length}}</p>
-        <div :class="'dropArea ' + dropAreaDisplay"
-            :style="'width:' + dropAreaSize[0] + 'px;height:' + dropAreaSize[1] + 'px;'"
-            v-on:dragover="over" v-on:dragleave="leave" v-on:drop="drop">
-            <p><span>drop here</span></p>
-        </div>
+        <drop-area :display="dropAreaDisplay" />
         <div class="bookContainer">
             <template v-for="(book, idx) in books">
                 <div :key="idx">
-                    <img class="bookImg" :src="book.book" draggable="true" />
+                    <img class="bookImg" :src="book.book" />
                 </div>
             </template>
         </div>
     </div>
 </template>
 <script>
+import DropArea from './DropArea.vue';
 export default {
     data: function() {
         return {
             dropAreaDisplay: '',
-            dropAreaSize: [],
-            dropAreaObserver: null
         };
     },
     computed: {
@@ -56,39 +51,11 @@ export default {
             }
         }
     },
+    components: { DropArea },
     methods: {
-        over: function(ev) {
-            ev.preventDefault();
-            ev.dataTransfer.dropEffect = 'move';
-        },
-        leave: function(ev) {
-            ev.preventDefault();
-        },
-        drop: function(ev) {
-            ev.stopPropagation();
-            const data = ev.dataTransfer.getData('tgt');
-            console.log(data);
-        },
         onToggleRight: function() {
             const current = this.$store.getters.rightMode;
             this.$store.dispatch('setRightMode', !current);
-        }
-    },
-    created: function() {
-        const resizeObserver = new ResizeObserver((entries) => {
-            entries.forEach(({target, contentRect}) => {
-                const w = parseInt(contentRect.width);
-                const h = parseInt(contentRect.height) + 20;
-                this.dropAreaSize = [w, h];
-            })
-        });
-        this.dropAreaObserver = resizeObserver;
-    },
-    mounted: function() {
-        if (this.$el !== void 0) { 
-            console.log('el found!');
-            const container = this.$el.querySelector('.bookContainer');
-            this.dropAreaObserver.observe(container);
         }
     }
 }
@@ -109,19 +76,5 @@ export default {
 }
 .bookImg {
     border: solid 1px darkgray;
-}
-.dropArea {
-    border: dashed 2px dimgray;
-    opacity: 0.5;
-    position: absolute;
-    background-color: aquamarine;
-}
-.dropArea > p {
-    text-align: center;
-    font-size: 32px;
-    font-weight: bolder;
-}
-.dropArea-hide {
-    display: none;
 }
 </style>
---
+++ src/DropArea.vue
@@ -0,0 +1,64 @@
+<template>
+    <div :class="'dropArea ' + display"
+        :style="'width:' + dropAreaSize[0] + 'px;height:' + dropAreaSize[1] + 'px;'"
+        v-on:dragover="over" v-on:dragleave="leave" v-on:drop="drop">
+        <p><span>drop here</span></p>
+    </div>
+</template>
+<script>
+export default {
+    props: ['display'],
+    data: function() {
+        return {
+            dropAreaSize: [],
+            dropAreaObserver: null
+        };
+    },
+    methods: {
+        over: function(ev) {
+            ev.preventDefault();
+            ev.dataTransfer.dropEffect = 'move';
+        },
+        leave: function(ev) {
+            ev.preventDefault();
+        },
+        drop: function(ev) {
+            ev.stopPropagation();
+            const data = ev.dataTransfer.getData('tgt');
+            console.log(data);
+        }
+    },
+    created: function() {
+        const resizeObserver = new ResizeObserver((entries) => {
+            entries.forEach(({target, contentRect}) => {
+                const w = parseInt(contentRect.width);
+                const h = parseInt(contentRect.height) + 20;
+                this.dropAreaSize = [w, h];
+            })
+        });
+        this.dropAreaObserver = resizeObserver;
+    },
+    mounted: function() {
+        if (this.$el !== void 0) { 
+            const container = this.$el.parentNode.querySelector('.bookContainer');
+            this.dropAreaObserver.observe(container);
+        }
+    }
+}
+</script>
+<style scoped>
+.dropArea {
+    border: dashed 2px dimgray;
+    opacity: 0.5;
+    position: absolute;
+    background-color: aquamarine;
+}
+.dropArea > p {
+    text-align: center;
+    font-size: 32px;
+    font-weight: bolder;
+}
+.dropArea-hide {
+    display: none;
+}
+</style>
